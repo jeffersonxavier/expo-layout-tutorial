96896ad38b82ce8f0c7c8ad198020da728accc0c
Jefferson Xavier <jeffersonx.xavier@gmail.com>
2019-08-30 11:28:50 -0300

    adding header elements

---
+++ src/components/header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import styles from './styles';
+
+const Header = () => (
+  <View style={styles.container}>
+    <Icon name="ios-arrow-back" size={24} style={styles.icon} />
+    <Text style={styles.title}>Profile</Text>
+    <Icon name="ios-more" size={24} style={styles.icon} />
+  </View>
+);
+
+export default Header;
