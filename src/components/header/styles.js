c4b38366b42c3d838ab2c1e74a6c163ded19055b
Jefferson Xavier <jeffersonx.xavier@gmail.com>
2019-08-30 11:39:55 -0300

    styles to header component

--- src/components/header/styles.js
+++ src/components/header/styles.js
@@ -1,9 +1,26 @@
 import { StyleSheet } from 'react-native';
+import { colors, fonts, metrics } from '../../styles';
 
 const styles = StyleSheet.create({
     container: {
+        backgroundColor: colors.white,
         flexDirection: 'row',
+        paddingTop: metrics.headerPadding,
+        height: metrics.headerHeight,
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingHorizontal: metrics.padding,
+        borderBottomWidth: 1,
+        borderBottomColor: colors.lighter,
     },
+
+    icon: {
+        color: colors.primary,
+    },
+
+    title: {
+        fontSize: fonts.big,
+    }
 });
 
 export default styles;
