0055c744bfe9e993d6a1172e67a0e94be1cb7563
Jefferson Xavier <jeffersonx.xavier@gmail.com>
2019-08-30 20:12:36 -0300

    creating subheader component

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { View } from 'react-native';
 import Header from './src/components/header';
+import SubHeader from './src/components/subheader';
 
 export default function App() {
   return (
     <View style={{flex: 1}}>
       <Header/>
+      <SubHeader/>
     </View>
   );
 }
---
+++ src/components/subheader/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import styles from './styles';
+
+const SubHeader = () => (
+    <View style={styles.container}>
+        <Text>SubHeader</Text>
+    </View>
+);
+
+export default SubHeader;
