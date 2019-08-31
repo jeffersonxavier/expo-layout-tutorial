d0d30504d44995320e9eebf6dde91cba68f424b9
Jefferson Xavier <jeffersonx.xavier@gmail.com>
2019-08-30 21:36:13 -0300

    creating tabs component

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, ScrollView } from 'react-native';
 import Header from './src/components/header';
 import SubHeader from './src/components/subheader';
 import ProductList from './src/components/product-list';
+import Tabs from './src/components/tabs';
 
 export default function App() {
   return (
-    <View style={{flex: 1}}>
+    <View style={{flex: 1, backgroundColor: '#F8F8FA'}}>
       <Header/>
-      <SubHeader/>
-      <ProductList/>
+      <ScrollView>
+        <SubHeader/>
+        <ProductList/>
+      </ScrollView>
+      <Tabs/>
     </View>
   );
 }
