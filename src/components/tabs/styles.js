c763b21475a2ef424b70d2521e0e6d5af5ec5bfc
Jefferson Xavier <jeffersonx.xavier@gmail.com>
2019-08-30 21:41:55 -0300

    fixing layout tabs

--- src/components/tabs/styles.js
+++ src/components/tabs/styles.js
@@ -3,6 +3,37 @@ import { colors, fonts, metrics } from '../../styles';
 
 const styles = StyleSheet.create({
 
+    container: {
+        backgroundColor: colors.white,
+        height: metrics.tabBarHeight,
+        paddingHorizontal: metrics.padding,
+        flexDirection: 'row',
+        borderTopColor: colors.lighter,
+        borderTopWidth: 1,
+        justifyContent: 'space-around',
+        alignItems: 'center',
+    },
+
+    icon: {
+        color: colors.light,
+    },
+
+    active: {
+        color: colors.primary,
+    },
+
+    mainIcon: {
+        color: colors.white,
+    },
+
+    main: {
+        width: 40,
+        height: 40,
+        borderRadius: 20,
+        backgroundColor: colors.primary,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
 });
 
 export default styles;
